df28f92522eb4e0063accc84b20dbc022df0c8fe
Rishab Patwari <patwariri@gmail.com>
2022-04-09 17:03:35 +0800

    feat: reenabled straight line trajectory through config

--- src/ee4308_hector/include/ee4308_hector/trajectory.hpp
+++ src/ee4308_hector/include/ee4308_hector/trajectory.hpp
@@ -5,5 +5,6 @@
 #define TRAJECTORY_HPP
 std::vector<Position> generate_full_trajectory(double x_begin, double y_begin, double goal_1_x, double goal_1_y, double goal_2_x, double goal_2_y);
 std::vector<Position> generate_trajectory(double x_begin, double y_begin, double goal_1_x, double goal_1_y);
+std::vector<Position> generate_straight_line_trajectory(double x_begin, double y_begin, double goal_1_x, double goal_1_y, double goal_2_x, double goal_2_y, double look_ahead_duration);
 
 #endif
